ecc14692023f210e5422a6efb74e3f90f0db2e88
Ali Nazari <ali.nazari1996@aol.com>
2025-02-15 14:20:38 +0330

    main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,17 @@
+# main.tf
+provider "aws" {
+  region = "us-west-2"
+}
 
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.example.id
+}
